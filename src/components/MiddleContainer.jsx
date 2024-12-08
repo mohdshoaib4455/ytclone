0053269fa2a323a901e5ad9dc9d0b29475d9c3c3
mohd shoaib <mohdshoaib4455@gmail.com>
2024-12-08 18:27:17 +0530

    add sidebar

--- src/components/MiddleContainer.jsx
+++ src/components/MiddleContainer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import SideBar from './Sidebar';
 import Main from './Main';
+import SideBar from './Sidebar';
 
 const MiddleContainer = () => {
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-lg-12 p-0 d-flex">
-          <SideBar />
+          <SideBar/>
           <Main />
         </div>
       </div>
